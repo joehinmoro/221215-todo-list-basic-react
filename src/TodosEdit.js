10ef95542aba4bca1ab7810866cb800f76627660
joehinmoro <joehinmoro@gmail.com>
2023-01-18 06:49:57 +0100

    edit route progress

---
+++ src/TodosEdit.js
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const TodosEdit = () => {
+    const { id } = useParams();
+    return <div className="container"></div>;
+};
+
+export default TodosEdit;
