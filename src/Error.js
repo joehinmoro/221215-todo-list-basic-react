f21af791bb6ebac3e3713e13374b7530edcc9908
joehinmoro <joehinmoro@gmail.com>
2023-01-27 12:59:14 +0100

    minor fix

--- src/Error.js
+++ src/Error.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Error = ({ error }) => {
     return (
-        <div className="container  text-center  mt-5">
+        <div className="container text-center mt-5">
             <div className="row justify-content-center">
                 <div className=" col-sm-12 col-md-10 col-lg-8 col-xl-6 border p-5 border-danger-subtle">
                     <h3 className="mt-3 text-danger">Error :(</h3>
